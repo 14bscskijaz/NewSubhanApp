2956f0e89faabd3f6e54bbebfaa67e2e1d1dc3e4
Ismail Tirmizi <ismailhero@gmail.com>
2024-11-28 18:50:59 +0500

    Modify busdto to recieve string. Remove compiled directories

--- BusServiceAPI/BusServiceAPI/Models/DTOs/BusDTO.cs
+++ BusServiceAPI/BusServiceAPI/Models/DTOs/BusDTO.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; } // Serial (integer and autoincrementing)
         public string BusNumber { get; set; } // varchar(9)
-        public BusTypeEnum BusType { get; set; } // Enum (Standard, Business)
+        public string BusType { get; set; } // Enum (Standard, Business)
         public string BusOwner { get; set; } // varchar(55)
         public string Description { get; set; } // varchar(255)
         public string BusStatus { get; set; } // varchar(20)

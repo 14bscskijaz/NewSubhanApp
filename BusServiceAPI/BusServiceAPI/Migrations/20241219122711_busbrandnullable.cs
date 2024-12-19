aacd197ef9043c9152a04d796f2df8c0f0f02310
Khizar <14bscskijaz@seecs.edu.pk>
2024-12-19 17:27:29 +0500

    bus brand made nullable

---
+++ BusServiceAPI/BusServiceAPI/Migrations/20241219122711_busbrandnullable.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusServiceAPI.Migrations
+{
+    public partial class busbrandnullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "BusBrand",
+                table: "Buses",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "BusBrand",
+                table: "Buses",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
